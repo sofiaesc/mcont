d28279cafae2169d61fa9471aa5210d4bfc4caaf
Sofía Escudero <ssfescudero@gmail.com>
2023-04-17 21:00:23 -0300

    tp1

---
+++ Unidad 1: Introducción/TP1/f.m
@@ -0,0 +1,72 @@
+# La función recibe el intervalo de tiempo y la posición actual de los nodos.
+function dy = f(t,y)
+
+# DATOS:
+# Carga sinusoidal (inciso b):
+A = 1;
+fs = 2;
+phi = 0;
+senoidal = A*sin(2*pi*fs*t+phi);
+P = [0,senoidal];
+
+# Carga uniforme (inciso a):
+P = [0,1];
+
+# CONDICIONES INICIALES:
+# xi = [x1,y1,x2,y2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10,x11,y11,x12,y12,x13,y13,x14,y14];
+xi = [0 0 40 0 0 10 10 10 30 10 40 10 0 20 10 20 20 20 30 20 40 20 10 40 20 40 30 40];
+
+# CONSTANTES RESORTES:
+k = [2 2 2 2 4 2 2 2 2 4 2 4 2 2 2 2 2 4];
+
+# MASAS:
+m = [0 40 0 10 30 40 0 10 20 30 40 10 20 30];
+
+# CÁLCULO DE FUERZAS:
+# Calculo una vez cada par y aplico Fij = -Fij
+F13 = fuerza(xi(1:2), xi(5:6), xi(1:2), y(2:3), k(1));
+F14 = fuerza(xi(1:2), xi(7:8), xi(1:2), y(4:5), k(2));
+F43 = fuerza(xi(7:8), xi(5:6), y(4:5), y(2:3), k(3));
+F48 = fuerza(xi(7:8), xi(15:16), y(4:5), y(12:13), k(4));
+F87 = fuerza(xi(15:16), xi(13:14), y(12:13), y(10:11), k(5));
+F73 = fuerza(xi(13:14), xi(5:6), y(10:11), y(2:3), k(6));
+F83 = fuerza(xi(15:16), xi(5:6), y(12:13), y(2:3), k(7));
+F712 = fuerza(xi(13:14), xi(23:24), y(10:11), y(20:21), k(8));
+F812 = fuerza(xi(15:16), xi(23:24), y(12:13), y(20:21), k(9));
+F89 = fuerza(xi(15:16), xi(17:18), y(12:13), y(14:15), k(10));
+F913 = fuerza(xi(17:18), xi(25:26), y(14:15), y(22:23), k(11));
+F1213 = fuerza(xi(23:24), xi(25:26), y(20:21), y(22:23), k(12));
+F129 = fuerza(xi(23:24), xi(17:18), y(20:21), y(14:15), k(13));
+F26 = fuerza(xi(3:4), xi(11:12), [y(1),xi(4)], y(8:9), k(14));
+F25 = fuerza(xi(3:4), xi(9:10), [y(1),xi(4)], y(6:7), k(15));
+F56 = fuerza(xi(9:10), xi(11:12), y(6:7), y(8:9), k(16));
+F510 = fuerza(xi(9:10), xi(19:20), y(6:7), y(16:17), k(17));
+F1011 = fuerza(xi(19:20), xi(21:22), y(16:17), y(18:19), k(18));
+F116 = fuerza(xi(21:22), xi(11:12), y(18:19), y(8:9), k(19));
+F106 = fuerza(xi(19:20), xi(11:12), y(16:17), y(8:9), k(20));
+F1114 = fuerza(xi(21:22), xi(27:28), y(18:19), y(24:25), k(21));
+F1014 = fuerza(xi(19:20), xi(27:28), y(16:17), y(24:25), k(22));
+F109 = fuerza(xi(19:20), xi(17:18), y(16:17), y(14:15), k(23));
+F1413 = fuerza(xi(27:28), xi(25:26), y(24:25), y(22:23), k(24));
+F149 = fuerza(xi(27:28), xi(17:18), y(24:25), y(14:15), k(25));
+
+# ARMADO DE SISTEMA DE ECUACIONES:
+dy = zeros(length(y),1);
+
+# Velocidades:
+dy(1:18) = y(26:44);
+# Posiciones:
+dy( = 1/m(1) * (F13 + F14);
+dy2 = 1/m(2) * (F25 + F26);
+dy3 = 1/m(3) * (-F13 - F43 - F83 - F73);
+dy4 = 1/m(4) * (-F14 + F43 + F48);
+dy5 = 1/m(5) * (-F25 + F56 + F510);
+dy6 = 1/m(6) * (-F26 - F56 - F116 - F106);
+dy7 = 1/m(7) * (F73 - F87 + F712);
+dy8 = 1/m(8) * (F83 - F48 + F87 + F89 + F812);
+dy9 = 1/m(9) * (-F89 - F129 + F913 - F149 - F109);
+dy10 = 1/m(10) * (-F510 + F106 + F109 + F1011 + F1014);
+dy11 = 1/m(11) * (F116 - F1011 + F1114);
+dy12 = 1/m(12) * (-F812 - F912 + F129 + F1213);
+dy13 = 1/m(13) * (-F913 - F1213 - F1413 + P);
+dy14 = 1/m(14) * (F149 - F1014 - F1114 + F1413);
--- Unidad 1: Introducción/TP1/resultado.m
+++ Unidad 1: Introducción/TP1/resultado.m
@@ -1,2 +1,58 @@
 # En este script, se resuelve el sistema con la ode y se obtiene el gif con
 # los resultados del ejercicio.
+
+# DATOS DE TIEMPO:
+tini = 0;
+tfin = 50;
+t=tini:tfin;
+
+# CONDICIONES INICIALES:
+# Posición:
+# Para el nodo 1, el soporte es inmovil. Mantengo condiciones iniciales.
+# Para el nodo 2, el soporte es móvil en x. Mantengo condición inicial en y.
+# xi_0 = [x2,x3,y3,x4,y4,x5,y5,x6,y6,x7,y7,x8,y8,x9,y9,x10,y10,x11,y11,x12,y12,x13,y13,x14,y14];
+xi_0 = [40 0 10 10 10 30 10 40 10 0 20 10 20 20 20 30 20 40 20 10 40 20 40 30 40];
+# Velocidad:
+vi_0 = [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0];
+# Vector completo:
+y = [xi_0, vi_0];
+
+# RESOLUCIÓN DEL SISTEMA DE ECUACIONES:
+[t,y] = ode23(@f,t,y);
+
+#------------------------------------------------------------
+# BARRA 13:
+for i = 1:tfin+1
+  # FALTA CAMBIAR VALORES EN Y(I,??):
+   F = fuerza(xi_0(20:21),xi_0(14:15),y(i,10:11),y(i,16:17),2);
+   F129_x(i)=F(1);
+   F129_y(i)=F(2);
+endfor
+
+figure(1)
+hold on
+grid on
+title('Barra 13 Fuerza')
+xlabel('Tiempo')
+ylabel('Fuerza')
+# Grafico fuerza en X para la barra:
+subplot(2,1,1)
+plot(t,F129_x)
+# Grafico fuerza en Y para la barra:
+subplot(2,1,2)
+plot(t,F129_y)
+
+# NODO 13:
+figure(2)
+hold on
+grid on
+title('Posicion X Nodo 6')
+xlabel('Tiempo');
+ylabel('Posición');
+# Grafico posición en X para el nodo:
+subplot(2,1,1)
+plot(t,y(1:tfin+1, 22));
+# Grafico posición en Y para el nodo:
+subplot(2,1,2)
+plot(t,y(1:tfin+1, 23));
+
