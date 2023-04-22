7632e844a9115b6c9c4d8bd408268381fd011201
Sofía Escudero <ssfescudero@gmail.com>
2023-04-22 15:40:54 -0300

    tp1 terminado

--- Unidad 1: Introducción/TP1/Código/fuerza.m
+++ Unidad 1: Introducción/TP1/Código/fuerza.m
@@ -1,12 +1,12 @@
 % FUNCIÓN PARA LA DEFINICIÓN DE LAS FUERZAS
 function F = fuerza(xi_0, xj_0, xi, xj, k)
   % INCISOS A Y B: Grandes deformaciones
-  %Ftot = k * (1 - sqrt((xj_0(1) - xi_0(1))^2 + (xj_0(2) - xi_0(2))^2)/sqrt((xj(1) - xi(1))^2 + (xj(2) - xi(2))^2));
-  %F(1) = Ftot * (xj(1) - xi(1)); % fuerza en x
-  %F(2) = Ftot * (xj(2) - xi(2)); % fuerza en y
+  Ftot = k * (1 - sqrt((xj_0(1) - xi_0(1))^2 + (xj_0(2) - xi_0(2))^2)/sqrt((xj(1) - xi(1))^2 + (xj(2) - xi(2))^2));
+  F(1) = Ftot * (xj(1) - xi(1)); % fuerza en x
+  F(2) = Ftot * (xj(2) - xi(2)); % fuerza en y
 
   % INCISO C: Pequeñas deformaciones
-  Ftot = k * (sqrt((xj(1) - xi(1))^2 + (xj(2) - xi(2))^2)/sqrt((xj_0(1) - xi_0(1))^2 + (xj_0(2) - xi_0(2))^2) - 1);
-  F(1) = Ftot * (xj_0(1) - xi_0(1)); % fuerza en x
-  F(2) = Ftot * (xj_0(2) - xi_0(2)); % fuerza en y
+  %Ftot = k * (sqrt((xj(1) - xi(1))^2 + (xj(2) - xi(2))^2)/sqrt((xj_0(1) - xi_0(1))^2 + (xj_0(2) - xi_0(2))^2) - 1);
+  %F(1) = Ftot * (xj_0(1) - xi_0(1)); % fuerza en x
+  %F(2) = Ftot * (xj_0(2) - xi_0(2)); % fuerza en y
 
